f4a6f08ecc009e812bdbc2169282cd6b04fa6f70
Deshy Dan <deshy733@gmail.com>
2023-07-21 00:35:03 +0200

    Added feature for filtering games by genre

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,15 @@ import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
+import { useState } from "react";
+import { Genre } from "./hooks/useGenre";
 
 function App() {
+    const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
+
+    const handleSelectGenre = (genre: Genre) => {
+        setSelectedGenre(genre);
+    };
     return (
         <>
             <Grid
@@ -12,21 +19,25 @@ function App() {
                     lg: `"nav nav" "aside main"`,
                 }}
                 templateColumns={{
-                    base:'1fr',
-                    lg:'200px 1fr'
+                    base: "1fr",
+                    lg: "200px 1fr",
                 }}
             >
-                <GridItem area="nav" >
-                <NavBar/>
+                <GridItem area="nav">
+                    <NavBar />
                 </GridItem>
                 <Show above="lg">
-                    <GridItem area="aside" paddingX={5} >
-                        <GenreList/>
+                    <GridItem area="aside" paddingX={5}>
+                        <GenreList
+                            onSelectGenre={(genres) =>
+                                handleSelectGenre(genres)
+                            }
+                        />
                     </GridItem>
                 </Show>
 
-                <GridItem area="main" >
-                <GameGrid/>
+                <GridItem area="main">
+                    <GameGrid selectedGenre={selectedGenre} />
                 </GridItem>
             </Grid>
         </>
--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -9,8 +9,9 @@ import {
 import { MdPhoneIphone } from "react-icons/md";
 import { SiNintendo } from "react-icons/si";
 import { BsGlobe } from "react-icons/bs";
-import { HStack, Icon, Text } from "@chakra-ui/react";
+import { HStack, Icon } from "@chakra-ui/react";
 import { Platform } from "../hooks/useGames";
+import { IconType } from "react-icons";
 
 
 interface Props {
@@ -32,7 +33,7 @@ const PlatformIconList = ({ platforms }: Props) => {
     return (
         <HStack marginY={1}>
             {platforms.map((platform) => (
-                <Icon as={iconMap[platform.slug]} color='gray.500' key={platform.slug} />
+                <Icon key={platform.id} as={iconMap[platform.slug]} color='gray.500' key={platform.slug} />
             ))}
         </HStack>
     );
