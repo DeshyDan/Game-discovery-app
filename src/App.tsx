7ce6893f27600627cd6dccdd1465a228ff23f0ee
Deshy Dan <deshy733@gmail.com>
2023-07-25 21:50:12 +0200

    Added dynamic page heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, HStack, Show } from "@chakra-ui/react";
+import { Box, Flex, Grid, GridItem, HStack, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
@@ -7,6 +7,7 @@ import { Genre } from "./hooks/useGenre";
 import PlatformSelector from "./components/PlatformSelector";
 import { Platform } from "./hooks/usePlatform";
 import SortSelector from "./components/SortSelector";
+import GameHeading from "./components/GameHeading";
 
 export interface GameQuery {
     genre: Genre | null;
@@ -49,20 +50,27 @@ function App() {
                 </Show>
 
                 <GridItem area="main">
-                    <HStack spacing={5} paddingLeft={10}>
-                        <PlatformSelector
-                            selectedPlatform={gameQuery.platform}
-                            onSelectPlatform={(platform) =>
-                                setGameQuery({ ...gameQuery, platform })
-                            }
-                        />
-                        <SortSelector
-                            currentSortOrder={gameQuery.sortOrder}
-                            onSelectSortOrder={(sortOrder) =>
-                                setGameQuery({ ...gameQuery, sortOrder })
-                            }
-                        />
-                    </HStack>
+                   
+                    <Box paddingLeft={2}>
+                        <GameHeading gameQuery={gameQuery}/>
+                        <Flex marginBottom={5} >
+                        
+                            <Box marginRight={5}>
+                                <PlatformSelector
+                                    selectedPlatform={gameQuery.platform}
+                                    onSelectPlatform={(platform) =>
+                                        setGameQuery({ ...gameQuery, platform })
+                                    }
+                                />
+                            </Box>
+                            <SortSelector
+                                currentSortOrder={gameQuery.sortOrder}
+                                onSelectSortOrder={(sortOrder) =>
+                                    setGameQuery({ ...gameQuery, sortOrder })
+                                }
+                            />
+                        </Flex>
+                    </Box>
 
                     <GameGrid gameQuery={gameQuery} />
                 </GridItem>
