e714cece44a58ffebe1f6867dea74d382fe9b080
Deshy Dan <deshy733@gmail.com>
2023-09-22 11:41:04 +0200

    Simplified the games query

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,14 @@ import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
 import { useState } from "react";
-import { Genre } from "./hooks/useGenre";
 import PlatformSelector from "./components/PlatformSelector";
 import { Platform } from "./hooks/usePlatform";
 import SortSelector from "./components/SortSelector";
 import GameHeading from "./components/GameHeading";
 
 export interface GameQuery {
-    genre: Genre | null;
-    platform: Platform | null;
+    genreId?: number;
+    platformId?: number;
     sortOrder: string;
     searchText: string;
 }
@@ -41,9 +40,12 @@ function App() {
                 <Show above="lg">
                     <GridItem area="aside" paddingX={5}>
                         <GenreList
-                            selectedGenre={gameQuery.genre}
+                            selectedGenreId={gameQuery.genreId}
                             onSelectGenre={(genre) =>
-                                setGameQuery({ ...gameQuery, genre })
+                                setGameQuery({
+                                    ...gameQuery,
+                                    genreId: genre.id,
+                                })
                             }
                         />
                     </GridItem>
@@ -55,9 +57,12 @@ function App() {
                         <Flex marginBottom={5}>
                             <Box marginRight={5}>
                                 <PlatformSelector
-                                    selectedPlatform={gameQuery.platform}
-                                    onSelectPlatform={(platform) =>
-                                        setGameQuery({ ...gameQuery, platform })
+                                    selectedPlatformId={gameQuery.platformId}
+                                    onSelectPlatform={(platform: Platform) =>
+                                        setGameQuery({
+                                            ...gameQuery,
+                                            platformId: platform.id,
+                                        })
                                     }
                                 />
                             </Box>
