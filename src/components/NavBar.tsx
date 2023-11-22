005e5762e6afefbf127ccda489cf0284dd3fcaac
Deshy Dan <deshy733@gmail.com>
2023-11-22 13:03:05 +0200

    Change routing for the navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,20 +1,21 @@
-import { HStack, Image } from '@chakra-ui/react';
-import logo from '../assets/logo.webp';
-import ColorModeSwitch from './ColorModeSwitch';
-import SearchInput from './SearchInput';
-
+import { HStack, Image } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
+    // const {setSearchText} = useGameQueryStore();
+    return (
+        <HStack padding="10px">
+            <Link to={"/"}>
+                <Image src={logo} boxSize="60px" objectFit={"cover"} />
+            </Link>
 
-  // const {setSearchText} = useGameQueryStore();
-  return (
-   <HStack  padding='10px'>
-    <Image src={logo} boxSize='60px'/>
-    <SearchInput/>
-    <ColorModeSwitch/>
-  
-   </HStack>
-  )
-}
+            <SearchInput />
+            <ColorModeSwitch />
+        </HStack>
+    );
+};
 
-export default NavBar
+export default NavBar;
